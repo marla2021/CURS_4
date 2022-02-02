fc4e83d6cbeaf5ba6e2329f827d07c33802c3d59
marla2021 <91145453+marla2021@users.noreply.github.com>
2022-02-02 20:17:54 +0300

    add app.py

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+# heroes = {
+#     "player": player,
+#     "enemy": enemy
+# }
+
+@app.route("/")
+def menu():
+    return render_template("index.html")
+
+@app.route("/choose-hero/")
+def hero_page():
+    return render_template ("hero_chosing.html")
 
 
-heroes = {
-    "player": player,
-    "enemy": enemy
-}
+if __name__ == "__main__":
+    app.run(port=8000)
